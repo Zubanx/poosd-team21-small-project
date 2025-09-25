54bc91f95fe6518a9991c5725a9c7b9159552053
Javier Chavez <cjavier0315@gmail.com>
2025-09-25 11:19:58 -0400

    Fix for AddContact.php

--- API/AddContact.php
+++ API/AddContact.php
@@ -9,7 +9,7 @@
     {
         returnWithError($conn->connect_error);
     } else {
-        $stmt = $conn->prepare("INSERT INTO Contacts (firstName, lastName, email, phoneNumber, address, userID) VALUES (?, ?, ?, ?, ?, ?) WHERE userID = ?");
+        $stmt = $conn->prepare("INSERT INTO Contacts (firstName, lastName, email, phoneNumber, address, userID) VALUES (?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("sssssi", $inData["firstName"], $inData["lastName"], $inData["email"], $inData["phoneNumber"], $inData["address"], $inData["userID"]);
         if($stmt->execute()){
             $last_id = $conn->insert_id;
