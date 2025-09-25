f8cfd02167c3a400bf17fe5cdbc2d24e9c8dfd18
Javier Chavez <cjavier0315@gmail.com>
2025-09-25 12:39:43 -0400

    Initial test of GetContact.php

---
+++ API/GetContact.php
@@ -0,0 +1,54 @@
+<?php
+
+$inData = getRequestInfo();
+$firstName = "";
+$lastName = "";
+$searchResults = "";
+
+$conn = new mysqli("localhost", "admin", "smallProjectUser", "Small_Project");
+if($conn->connect_error){
+    returnWithError($conn->connect_error);
+}else
+{
+    $stmt = $conn->prepare("SELECT * FROM Contacts WHERE ID = ?");
+    $stmt->bind_param("i", $inData["id"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($row = $result->fetch_assoc())
+    {
+        $searchResults = [
+            "ID"        => (int)$row["ID"],
+            "firstName" => $row["firstName"],
+            "lastName"  => $row["lastName"],
+            "phone"     => $row["phone"],
+            "email"     => $row["email"],
+            "timestamp" => $row["timestamp"]
+        ];
+        returnWithInfo($searchResults);
+    } else {
+        returnWithError("No Records Found");
+    }
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo(){
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj)
+{
+    header('Content-type:application/json');
+    echo $obj;
+}
+function returnWithError( $err )
+{
+    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+function returnWithInfo( $searchResults )
+{
+    $retValue = ["results" => [$searchResults], "error" => ""];
+    sendResultInfoAsJson(json_encode($retValue));
+}
+?>
