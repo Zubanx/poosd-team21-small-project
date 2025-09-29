822969e0aed649c957fca400d3ea1a0c1e0ece03
Javier Chavez <cjavier0315@gmail.com>
2025-09-29 10:54:31 -0400

    Fixed error with wrong var name for phone number in SearchContact.php

--- API/SearchContact.php
+++ API/SearchContact.php
@@ -22,7 +22,7 @@
                                         $searchResults .= ",";
                                 }
                                 $searchCount++;
-                                $searchResults .= '{"ID":"' . $row["ID"] . '",' .  '"First Name":"' . $row["firstName"] . '",' .  '"Last Name":"' . $row["lastName"] . '",' .  '"Number":"' . $row["phoneNumber"] . '",' .  '"Email":"' . $row["email"] . '",' .  '"Date":"' . $row["timestamp"] . '"}';
+                                $searchResults .= '{"ID":"' . $row["ID"] . '",' .  '"First Name":"' . $row["firstName"] . '",' .  '"Last Name":"' . $row["lastName"] . '",' .  '"Number":"' . $row["number"] . '",' .  '"Email":"' . $row["email"] . '",' .  '"Date":"' . $row["timestamp"] . '"}';
 			}
 		}else{
 			returnWithError("No Records Found");
